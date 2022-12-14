201850cc0b844d715169ecb37a38cfaf2d757f5a
bkammer <bkammer@stud.hs-heilbronn.de>
2022-12-14 16:22:44 +0100

    Updated room 2 and 3 and created template rooms for 0, 1 and 4.

--- src/home.js
+++ src/home.js
@@ -1,8 +1,6 @@
 // Check status of all rooms
 for (let i = 0; i < rooms.length; i++) {
   room = document.getElementById("icon-room" + i);
-  console.log(i + " " + room);
-  console.log(getRoomStatus(i));
   if (getRoomStatus(i)) {
     room.style.visibility = "visible";
   }
