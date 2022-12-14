18b401949fa5a810be7670179523bf47081aa06e
bkammer <bkammer@stud.hs-heilbronn.de>
2022-12-14 18:33:09 +0100

    Changed icons

--- src/home.js
+++ src/home.js
@@ -2,6 +2,6 @@
 for (let i = 0; i < rooms.length; i++) {
   room = document.getElementById("icon-room" + i);
   if (getRoomStatus(i)) {
-    room.style.visibility = "visible";
+    room.style.display = "block";
   }
 }
