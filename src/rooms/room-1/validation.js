f68caa4f5b69ddc76a1f34dd6a6afd2386f9f69a
bkammer <bkammer@stud.hs-heilbronn.de>
2023-01-26 09:05:01 +0100

    Canged alert timeout from 5 seconds to 10 seconds.

--- src/rooms/room-1/validation.js
+++ src/rooms/room-1/validation.js
@@ -18,7 +18,7 @@ cform.addEventListener("submit", (e) => {
     setTimeout(() => {
       currentAlert.remove();
       window.location.assign("/home.html");
-    }, 5000);
+    }, 10000);
     return;
   }
 
@@ -32,7 +32,7 @@ cform.addEventListener("submit", (e) => {
     setTimeout(() => {
       currentAlert.remove();
       window.location.assign("/home.html");
-    }, 5000);
+    }, 10000);
 
   } else {
     var oldAlert = document.getElementById("alert");
@@ -42,6 +42,6 @@ cform.addEventListener("submit", (e) => {
     var currentAlert = document.getElementById("alert");
     setTimeout(() => {
       currentAlert.remove();
-    }, 5000);
+    }, 10000);
   }
 });
